9501ecc39f806ac3e1a5fd9a73cdd259b3bb8d2b
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-25 15:12:55 +0700

    style(table): modernize user selection panel

--- front-end/src/features/users/component/side_menu/UserList.jsx
+++ front-end/src/features/users/component/side_menu/UserList.jsx
@@ -1,43 +1,24 @@
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import SideSelectionList from "../../../../component/table/SideSelectionList";
 
-const UserList = ({ data = [], selectRow, onSelectRow,getControlLabel,name }) => {
+const UserList = ({ data = [], selectRow = {}, onSelectRow, getControlLabel, name }) => {
+  const items = data[0]?.data || [];
+
   return (
-    <Box>
-      <Typography variant="h6" fontWeight={"bold"} ml={1}>
-        *{getControlLabel("mtxt_user","User")}
-      </Typography>
-      <List sx={{ padding: 0}}>
-        {data[0]?.data.map((user) => (
-          <ListItem key={`${user.user_code}-${user.department_code}-${user.factory_code}`} disablePadding>
-            <ListItemButton
-              selected={`${selectRow.user_code}-${selectRow.department_code}-${selectRow.factory_code}` === `${user.user_code}-${user.department_code}-${user.factory_code}`}
-              onClick={() => onSelectRow(user)}
-              sx={{
-                "&.Mui-selected": {
-                  backgroundColor: "yellow",
-                  color: "black",
-                },
-                "&.Mui-selected:hover": {
-                  backgroundColor: "#686868",
-                },
-              }}
-            >
-              <ListItemText
-                primary={user.user_code}
-                secondary={user[name]}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    <SideSelectionList
+      title={getControlLabel("mtxt_user", "User")}
+      items={items}
+      getKey={(item) => `${item.user_code}-${item.department_code}-${item.factory_code}`}
+      isSelected={(item) =>
+        `${selectRow?.user_code}-${selectRow?.department_code}-${selectRow?.factory_code}` ===
+        `${item.user_code}-${item.department_code}-${item.factory_code}`
+      }
+      onSelect={onSelectRow}
+      getPrimary={(item) => item.user_code}
+      getSecondary={(item) => item[name]}
+      Icon={PersonRoundedIcon}
+    />
   );
 };
+
 export default UserList;
