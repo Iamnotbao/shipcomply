1989ac19b5224aa1b8d5ca4072f5e41602727a48
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-25 11:25:16 +0700

    feat(ui): apply global mui theme

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,16 +1,22 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 import "./i18n";
-import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ColumnTranslationProvider } from "./context/ColumnTranslationContext";
-createRoot(document.getElementById("root")).render(
+import appTheme from "./theme/appTheme";
 
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={appTheme}>
+    <CssBaseline />
     <BrowserRouter>
       <AuthProvider>
         <ColumnTranslationProvider>
-        <App />
+          <App />
         </ColumnTranslationProvider>
       </AuthProvider>
     </BrowserRouter>
+  </ThemeProvider>,
 );
