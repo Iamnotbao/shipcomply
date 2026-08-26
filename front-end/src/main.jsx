7cf3db9a94455ef8a1ac23281da5c219a843bfb1
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-26 08:42:41 +0700

    revert(ui): load original visual restore stylesheet

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n";
+import "./styles/legacyUiRestore.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ColumnTranslationProvider } from "./context/ColumnTranslationContext";
