282c58aec542d06dd546d7faf672dceca5bd0bb6
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-26 08:41:40 +0700

    revert(ui): restore original global MUI styling

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,23 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 import "./i18n";
-import "./styles/formFallback.css";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ColumnTranslationProvider } from "./context/ColumnTranslationContext";
-import appTheme from "./theme/appTheme";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={appTheme}>
-    <CssBaseline />
-    <BrowserRouter>
-      <AuthProvider>
-        <ColumnTranslationProvider>
-          <App />
-        </ColumnTranslationProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </ThemeProvider>,
+  <BrowserRouter>
+    <AuthProvider>
+      <ColumnTranslationProvider>
+        <App />
+      </ColumnTranslationProvider>
+    </AuthProvider>
+  </BrowserRouter>,
 );
