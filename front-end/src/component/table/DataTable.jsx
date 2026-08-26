fcfe2d3415121cb957197e15c51985b8cd31aa10
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-26 08:41:54 +0700

    revert(ui): use legacy table presentation

--- front-end/src/component/table/DataTable.jsx
+++ front-end/src/component/table/DataTable.jsx
@@ -1,11 +1,3 @@
 import DataTableLegacy from "./DataTableLegacy";
-import DataTableModern from "./DataTableModern";
-import { canUseModernDataTable } from "./modernTableConfig";
 
-export default function DataTable(props) {
-  return canUseModernDataTable(props) ? (
-    <DataTableModern {...props} />
-  ) : (
-    <DataTableLegacy {...props} />
-  );
-}
+export default DataTableLegacy;
